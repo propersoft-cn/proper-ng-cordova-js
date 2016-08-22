dce443bdf4a6e3372cf69929fc6e61e3473d69d2
shendongsheng <shendongsheng@propersoft.cn>
2016-08-22 15:03:01 +0800

    添加推送相关接口

--- proper-ng-cordova.js
+++ proper-ng-cordova.js
@@ -3,8 +3,10 @@
 		'properNgCordova.plugins'
 	]);
 	angular.module('properNgCordova.plugins', [
-		'properNgCordova.plugins.kvstore'
+		'properNgCordova.plugins.kvstore',
+		'properNgCordova.plugins.properpush'
 	]);
+	//加密键值对存储
 	angular.module('properNgCordova.plugins.kvstore', [])
 		.factory('$properKvstore', ['$q', '$window', function($q, $window) {
 			return {
@@ -24,6 +26,58 @@
 					var d = $q.defer();
 
 					$window.plugins.kvstore.kvGets(filename, keys, function(success) {
+						d.resolve(success);
+					}, function(error) {
+						d.reject(error);
+					});
+
+					return d.promise;
+				}
+			};
+		}]);
+
+	//推送sdk集成
+	angular.module('properNgCordova.plugins.properpush', [])
+		.factory('$properProperpush', ['$q', '$window', function($q, $window) {
+			return {
+				init: function(kvs) {
+					var d = $q.defer();
+
+					$window.plugins.properpush.init(kvs, function(success) {
+						d.resolve(success);
+					}, function(error) {
+						d.reject(error);
+					});
+
+					return d.promise;
+				},
+				bindUserid: function(kvs) {
+					var d = $q.defer();
+
+					$window.plugins.properpush.bindUserid(kvs, function(success) {
+						d.resolve(success);
+					}, function(error) {
+						d.reject(error);
+					});
+
+					return d.promise;
+				},
+				unBindUserid: function() {
+					var d = $q.defer();
+					//取消userid与设备的绑定关系，其时就是将userid置为空
+					var kvs={"userid":"","otherInfo":""};
+					$window.plugins.properpush.bindUserid(kvs, function(success) {
+						d.resolve(success);
+					}, function(error) {
+						d.reject(error);
+					});
+
+					return d.promise;
+				},
+				getDeviceInfo: function() {
+					var d = $q.defer();
+
+					$window.plugins.properpush.getDeviceInfo(function(success) {
 						d.resolve(success);
 					}, function(error) {
 						d.reject(error);
